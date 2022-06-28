e33e8fabcb13e373f2789311b0499b32813eefdc
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-28 19:41:34 +0530

    ADDED: User's first name validation is checked

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-
     public static void main(String[] args) {
 
         System.out.println("Welcome to user registration program");
