d46ba803c6bcfbe78db2c5991a747126cd2f86ff
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-28 20:24:41 +0530

    ADDED: User's mobile number validation is done

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -14,5 +14,8 @@ public class UserRegistration {
 
         boolean emailId = Pattern.matches("[a-z][a-z A-Z 0-9]+[@][a-z]+[.][a-z]{2,5}", "akankshajadhav2999@gmail.com");//true (a to z one time then a to z or A to Z or O to 9 multiple times then special character "@" then a to z multiple times then special character "." and lastly a to z minimum 2 times and maximum 5 times)
         System.out.println("Email Id : " + emailId);
+
+        boolean mobileNumber = Pattern.matches("[+]91 [6789][0-9]{9}", "+91 8828166460");//true (a to z one time then a to z or A to Z or O to 9 multiple times then special character "@" then a to z multiple times then special character "." and lastly a to z minimum 2 times and maximum 5 times)
+        System.out.println("Mobile Number : " + mobileNumber);
     }
 }
