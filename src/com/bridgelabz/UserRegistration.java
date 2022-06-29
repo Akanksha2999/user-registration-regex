66a6258b91e17a2209c98c43549a03eb75fd58af
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-29 17:06:00 +0530

    ADDED: All valid and invalid email samples validation done

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -30,5 +30,58 @@ public class UserRegistration {
 
         boolean password_4 = Pattern.matches("(?=.*[A-Z])(?=.*\\d)(?=.*[$&+,:;=?@#|'<>.-^*()%!])[a-z A-Z 0-9 $&+,:;=?@#|'<>.-^*()%!]{8,}", "AkankshaJadhav@99 ");//true (Atleast 1 special character in password)
         System.out.println("Password Number 4 : " + password_4);
+        System.out.println("");
+
+        System.out.println("All valid email samples validation is as follows : ");
+        boolean emailSample_1 = Pattern.matches("[a-z]+[@][a-z]+[.][a-z]{3,5}", "abc@yahoo.com");//true
+        System.out.println("Email Id_1 : " + emailSample_1);
+        boolean emailSample_2 = Pattern.matches("[a-z]+[-][0-9]+[@][a-z]+[.][a-z]{3,5}", "abc-100@yahoo.com");//true
+        System.out.println("Email Id_2 : " + emailSample_2);
+        boolean emailSample_3 = Pattern.matches("[a-z]+[.][0-9]+[@][a-z]+[.][a-z]{3,5}", "abc.100@yahoo.com");//true
+        System.out.println("Email Id_3 : " + emailSample_3);
+        boolean emailSample_4 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{3,5}", "abc111@abc.com");//true
+        System.out.println("Email Id_4 : " + emailSample_4);
+        boolean emailSample_5 = Pattern.matches("[a-z]+[-][0-9]+[@][a-z]+[.][a-z]{3,5}", "abc-100@abc.net");//true
+        System.out.println("Email Id_5 : " + emailSample_5);
+        boolean emailSample_6 = Pattern.matches("[a-z]+[.][0-9]+[@][a-z]+[.][a-z]{3,5}[.][a-z]{2,5}", "abc.100@abc.com.au");//true
+        System.out.println("Email Id_6 : " + emailSample_6);
+        boolean emailSample_7 = Pattern.matches("[a-z]+[@][0-9][.][a-z]{3,5}", "abc@1.com");//true
+        System.out.println("Email Id_7 : " + emailSample_7);
+        boolean emailSample_8 = Pattern.matches("[a-z]+[@][a-z]{5,}[.][a-z]{3,}[.][a-z]{3,}", "abc@gmail.com.com");//true
+        System.out.println("Email Id_8 : " + emailSample_8);
+        boolean emailSample_9 = Pattern.matches("[a-z]+[-][.][+][:][0-9]+[@][0-9][a-z]{5,}[.][a-z]{3,}[.][a-z]{3,}[.][a-z]{2,}[.][a-z]{3,}", "abc-.+:100@1gmail.com.com.au.net");//true
+        System.out.println("Email Id_9 : " + emailSample_9);
+
+        System.out.println("");
+
+        System.out.println("All the invalid email samples validation ia as follows : ");
+        boolean invalidEmailSample_1 = Pattern.matches("[a-z]+[@][a-z]+[.][a-z]{2,5}", "abc");//false
+        System.out.println("Email Id_1 : " + invalidEmailSample_1);
+        boolean invalidEmailSample_2 = Pattern.matches("[a-z]+[@][a-z]+[.][a-z]{2,5}", "abc@.com.my");//false
+        System.out.println("Email Id_2 : " + invalidEmailSample_2);
+        boolean invalidEmailSample_3 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc123@gmail.a");//false
+        System.out.println("Email Id_3 : " + invalidEmailSample_3);
+        boolean invalidEmailSample_4 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc123@.com");//false
+        System.out.println("Email Id_4 : " + invalidEmailSample_4);
+        boolean invalidEmailSample_5 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc123@.com.com");//false
+        System.out.println("Email Id_5 : " + invalidEmailSample_5);
+        boolean invalidEmailSample_6 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", ".abc@abc.com");//false
+        System.out.println("Email Id_6 : " + invalidEmailSample_6);
+        boolean invalidEmailSample_7 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc()*@gmail.com");//false
+        System.out.println("Email Id_7 : " + invalidEmailSample_7);
+        boolean invalidEmailSample_8 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc@%*.com");//false
+        System.out.println("Email Id_8 : " + invalidEmailSample_8);
+        boolean invalidEmailSample_9 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc..2002@gmail.com");//false
+        System.out.println("Email Id_9 : " + invalidEmailSample_9);
+        boolean invalidEmailSample_10 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc.@gmail.com");//false
+        System.out.println("Email Id_10 : " + invalidEmailSample_10);
+        boolean invalidEmailSample_11 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc@abc@gmail.com");//false
+        System.out.println("Email Id_11 : " + invalidEmailSample_11);
+        boolean invalidEmailSample_12 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc@gmail.com.1a");//false
+        System.out.println("Email Id_12 : " + invalidEmailSample_12);
+        boolean invalidEmailSample_13 = Pattern.matches("[a-z]+[0-9]+[@][a-z]+[.][a-z]{2,5}", "abc@gmail.com.aa.au");//false
+        System.out.println("Email Id_13 : " + invalidEmailSample_13);
+
+
     }
 }
